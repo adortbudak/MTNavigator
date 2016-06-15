2c549955b156c9c5e03ff3185b34b9f587912f05
Alper Dortbudak <adortbudak@outlook.com>
2016-06-15 08:07:58 -0500

    added ignore file

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: "app-main",
+    template: `
+        <div>
+            <h3>Application main place holder</h3>
+        </div>
+    `
+})
+
+export class AppComponent{
+
+}
